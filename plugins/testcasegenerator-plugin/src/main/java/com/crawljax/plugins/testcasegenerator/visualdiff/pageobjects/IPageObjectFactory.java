b4144e7fc0354aca115df641d3bc78129c148b2a
abhijeetTehra <abhijeet.t@gaiansolutions.com>
2023-07-27 17:51:51 +0530

    cleaned

--- plugins/testcasegenerator-plugin/src/main/java/com/crawljax/plugins/testcasegenerator/visualdiff/pageobjects/IPageObjectFactory.java
+++
@@ -1,8 +0,0 @@
-package com.crawljax.plugins.testcasegenerator.visualdiff.pageobjects;
-
-import org.opencv.core.Mat;
-
-public interface IPageObjectFactory {
-
-    PageObject makePageObject(Mat image, int x, int y, int width, int height);
-}
